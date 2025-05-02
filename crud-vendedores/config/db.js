d7fa3f0ebe24d8fa7544eceee1dcfd182c3b6d76
Samuel Rivas <samuelrivasm07@gmail.com>
2025-05-02 09:42:05 -0500

    Update db.js
    CREDENCIALES XD

--- crud-vendedores/config/db.js
+++ crud-vendedores/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "tramway.proxy.rlwy.net",
+  host: "nozomi.proxy.rlwy.net",
   user: "root",
-  password: "kQFQPMPIkAojatyUTITqzDGeSfpWuYfg",
+  password: "cwmYThLLqFvKXknDUtrNpYDCSLrCmCbY",
   database: "railway",
-  port: 35910,
+  port: 40425,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
